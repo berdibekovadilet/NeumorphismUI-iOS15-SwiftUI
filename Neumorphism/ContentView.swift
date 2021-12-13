601773cfdaa543ec5aa84180f3e33e41a3fea588
Adilet <berdibekovadilet@icloud.com>
2021-12-13 11:28:42 +0300

    Initial Commit

---
+++ Neumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by Adilet Berdibekov on 13.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neumorphism/NeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismApp.swift
+//  Neumorphism
+//
+//  Created by Adilet Berdibekov on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
